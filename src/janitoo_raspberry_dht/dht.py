26d4c8c1e3c412ccc023b21f3613ee0e8f7bf67c
bibi21000 <bibi21000@gmail.com>
2016-01-26 20:35:54 +0100

    updates

---
+++ src/janitoo_raspberry_dht/dht.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""The Raspberry http thread
+
+Server files using the http protocol
+
+"""
+
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015 Sébastien GALLET aka bibi21000"
+
+import logging
+logger = logging.getLogger(__name__)
+import os, sys
+import threading
+
+from janitoo.thread import JNTBusThread, BaseThread
+from janitoo.options import get_option_autostart
+from janitoo.utils import HADD
+from janitoo.node import JNTNode
+from janitoo.value import JNTValue
+from janitoo.component import JNTComponent
+from janitoo_raspberry_i2c.i2c_bus import I2CBus
+
+##############################################################
+#Check that we are in sync with the official command classes
+#Must be implemented for non-regression
+from janitoo.classes import COMMAND_DESC
+
+COMMAND_WEB_CONTROLLER = 0x1030
+COMMAND_WEB_RESOURCE = 0x1031
+COMMAND_DOC_RESOURCE = 0x1032
+
+assert(COMMAND_DESC[COMMAND_WEB_CONTROLLER] == 'COMMAND_WEB_CONTROLLER')
+assert(COMMAND_DESC[COMMAND_WEB_RESOURCE] == 'COMMAND_WEB_RESOURCE')
+assert(COMMAND_DESC[COMMAND_DOC_RESOURCE] == 'COMMAND_DOC_RESOURCE')
+##############################################################
+
+def make_dht(**kwargs):
+    return DHTComponent(**kwargs)
+
+SENSORS = { '11': Adafruit_DHT.DHT11,
+            '22': Adafruit_DHT.DHT22,
+            '2302': Adafruit_DHT.AM2302 }
+
+class DHTComponent(JNTComponent):
+    """ A generic component for gpio """
+
+    def __init__(self, bus=None, addr=None, **kwargs):
+        """
+        """
+        oid = kwargs.pop('oid', 'rpibasic.dht')
+        name = kwargs.pop('name', "Input")
+        product_name = kwargs.pop('product_name', "DHT")
+        product_type = kwargs.pop('product_type', "Temperature/humidity sensor")
+        product_manufacturer = kwargs.pop('product_manufacturer', "Janitoo")
+        JNTComponent.__init__(self, oid=oid, bus=bus, addr=addr, name=name,
+                product_name=product_name, product_type=product_type, product_manufacturer="Janitoo", **kwargs)
+        logger.debug("[%s] - __init__ node uuid:%s", self.__class__.__name__, self.uuid)
+
+        uuid="pin"
+        self.values[uuid] = self.value_factory['config_integer'](options=self.options, uuid=uuid,
+            node_uuid=self.uuid,
+            help='The pin number on the board',
+            label='Pin',
+            default=1,
+        )
+        uuid="sensor"
+        self.values[uuid] = self.value_factory['config_integer'](options=self.options, uuid=uuid,
+            node_uuid=self.uuid,
+            help='The sensor type : 11,22,2302',
+            label='Type',
+            default=11,
+        )
+        uuid="temperature"
+        self.values[uuid] = self.value_factory['sensor_temperature'](options=self.options, uuid=uuid,
+            node_uuid=self.uuid,
+            help='The temperature',
+            label='Temp',
+            get_data_cb=self.temperature,
+        )
+        poll_value = self.values[uuid].create_poll_value()
+        self.values[poll_value.uuid] = poll_value
+
+        uuid="humidity"
+        self.values[uuid] = self.value_factory['sensor_humidity'](options=self.options, uuid=uuid,
+            node_uuid=self.uuid,
+            help='The humidity',
+            label='Hum',
+            get_data_cb=self.humidity,
+        )
+        poll_value = self.values[uuid].create_poll_value()
+        self.values[poll_value.uuid] = poll_value
+
+    def temperature(self, node_uuid, index):
+        res = os.popen('vcgencmd measure_temp').readline()
+        ret = None
+        try:
+            ret = float(self.re_nondecimal.sub('', res))
+        except ValueError:
+            logger.exception('Exception when retrieving temperature')
+            ret = None
+        return ret
+
+    def humidity(self, node_uuid, index):
+        res = os.popen('vcgencmd measure_temp').readline()
+        ret = None
+        try:
+            ret = float(self.re_nondecimal.sub('', res))
+        except ValueError:
+            logger.exception('Exception when retrieving humidity')
+            ret = None
+        return ret
