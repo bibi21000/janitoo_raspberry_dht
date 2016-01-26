387b8fb7c6248714d3aa0d15bbebe4a13da4c92b
bibi21000 <bibi21000@gmail.com>
2016-01-26 18:38:54 +0100

    update setup

--- setup.py
+++ setup.py
@@ -96,11 +96,14 @@ setup(
                      'janitoo',
                      'janitoo_raspberry_i2c',
                      #~ 'janitoo_buses == %s'%janitoo_version,
-                     'Adafruit_Python_DHT',
+                     'Adafruit-GPIO',
+                     'Adafruit_DHT',
                     ],
     dependency_links = [
       'https://github.com/bibi21000/janitoo/archive/master.zip#egg=janitoo',
       'https://github.com/bibi21000/janitoo_raspberry_i2c/archive/master.zip#egg=janitoo_rapsberry_i2c',
+      'https://github.com/adafruit/Adafruit_Python_GPIO/archive/master.zip#egg=Adafruit-GPIO',
+      'https://github.com/adafruit/Adafruit_Python_DHT/archive/master.zip#egg=Adafruit_DHT',
     ],
     entry_points = janitoo_entry_points,
 )
