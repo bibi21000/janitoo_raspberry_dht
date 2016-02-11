75f1d8b6c927aaaafde244a1be5fd23d7ca617b1
bibi21000 <bibi21000@gmail.com>
2016-02-11 22:04:42 +0100

    Auto-commit

--- tests/test_server.py
+++ tests/test_server.py
@@ -52,8 +52,6 @@ COMMAND_DISCOVERY = 0x5000
 assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
 ##############################################################
 
-JNTTServer.skipRasperryTest()
-
 class TestPiSerser(JNTTServer, JNTTServerCommon):
     """Test the pi server
     """
@@ -72,5 +70,14 @@ class TestPiSerser(JNTTServer, JNTTServerCommon):
         self.assertBroadcastRequest(cmd_class=COMMAND_DISCOVERY, uuid='request_info_nodes', client_hadd=HADD%(9999,0))
         self.stop()
 
+    def test_120_server_start_no_error_in_log(self):
+        self.onlyRasperryTest()
+        self.start()
+        try:
+            time.sleep(60)
+            self.assertInLogfile('Found heartbeats in timeout')
+            self.assertNotInLogfile('^ERROR ')
+        finally:
+            self.stop()
 
 
